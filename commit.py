ea2a81793604524bdaa5ae537d3506e71b2eaf62
raghhav <raghhavdturki@gmail.com>
2021-02-09 11:09:55 +0530

    Updating the repository

---
+++ commit.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  9 10:45:10 2021
+
+@author: raghhav
+"""
+
+import subprocess as cmd
+
+cp = cmd.run("git add .", check=True, shell=True)
+#print(cp)
+
+response = input("Do you want to use the default message for this commit?([y]/n)\n")
+message = "Updating the repository"
+
+if response.startswith('n'):
+    message = input("What message you want?\n")
+
+cp = cmd.run(f"git commit -m '{message}'", check=True, shell=True)
+cp = cmd.run("git push origin main", check=True, shell=True)
