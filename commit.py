74ba927c321a22855d2e9986012835e4eee8298b
raghhav <raghhavdturki@gmail.com>
2021-02-09 16:18:19 +0530

    Updating the repository

--- commit.py
+++ commit.py
@@ -8,7 +8,12 @@ Created on Tue Feb  9 10:45:10 2021
 
 import subprocess as cmd
 
-cp = cmd.run("git add .", check=True, shell=True)
+res = input("Do you wish to add all files?([y]/n)\n")
+if res.startswith('n'):
+    file = input("Enter file name\n")
+    cp = cmd.run(f"git add '{file}'",check=True, shell=True)
+else:
+    cp = cmd.run("git add .", check=True, shell=True)
 #print(cp)
 
 response = input("Do you want to use the default message for this commit?([y]/n)\n")
